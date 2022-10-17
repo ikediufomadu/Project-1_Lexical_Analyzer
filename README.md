584df40d554540f0dd93cb4635273d0043dbd881
ikedi <ikediufomadu@gmail.com>
2022-10-17 13:09:25 -0400

    Updated Functions v.9 -All functions operational -Need to fix repetitive output -Need to address position function

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Ikedi Ufomadu
 ## Development Specifications
 ### Java 17
-###
-###
+### Javac 17.0.2
+### IntelliJ IDEA 2021.3.3 (Community Edition)
 
-#### Compile using the command:
-`javac lexicalAnalyzer`
-#### Run using the command:
-`java lexicalAnalyzer`
+# Compile and run via IDE
 
-## Program Useage
+## Program Usage
+* User is prompted to enter file or quit
+* Program outputs tokenized lexemes
+* On event there is an error a report is generated with the position and character then the program exits
--- src/lexicalAnalyzer.java
+++ src/lexicalAnalyzer.java
@@ -1,12 +1,11 @@
 //Ikedi Ufomadu
 //Lexical Analyzer
-//
 
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
 
 public class lexicalAnalyzer {
     //Variables
@@ -15,8 +14,7 @@ public class lexicalAnalyzer {
     private static int currentCharInLine = 0;
     private static String currentTokenValue = "";
     private static String currentTokenRead = "";
-    private static boolean lexErrorReported = false;
-    private static List<String> words = new ArrayList<>();
+    private static final List<String> words = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
@@ -27,8 +25,11 @@ public class lexicalAnalyzer {
             System.exit(0);
         }
         reader(fileName);
-        value();
-        //kindHelper();
+        for (String s: words) {
+            isKeyword(s);
+            kind();
+            System.out.println(position() + ": " + kind() + " " + value());
+        }
     }
     //Opens a text file if it exists and reads it
     public static void reader(String filenameToRead) throws IOException {
@@ -76,106 +77,112 @@ public class lexicalAnalyzer {
         }
         return false;
     }
-    //Converts characters passed by reader method to words
+
+    //Converts characters passed by reader method to words, also creates a new word when it takes in certain symbols
     private static void charToWord(char[] charHolder) {
-        String newWord = "";
+        StringBuilder newWord = new StringBuilder();
         for (int i = 0; i < charHolder.length; i++) {
             for (int j = i + 1; j < charHolder.length; j++){
                 if (charHolder[i] == '/' && charHolder[j] == '/') {
                     return;
                 }
             }
-            if (charHolder[i] != ' ') {
-                newWord = newWord + charHolder[i];
+            //Adds these symbols individually to allow access to the char adjacent to it
+            if (charHolder[i] == ':') {
+                words.add(newWord.toString());
+                newWord = new StringBuilder();
+            }
+            if (charHolder[i] != ' ' && charHolder[i] != ';' && charHolder[i] != '(' && charHolder[i] != ')') {
+                newWord.append(charHolder[i]);
                  if (i == charHolder.length - 1) {
-                     words.add(newWord);
+                     words.add(newWord.toString());
                  }
             }
             else {
-                words.add(newWord);
-                newWord = "";
+                words.add(newWord.toString());
+                newWord = new StringBuilder();
             }
         }
     }
-    private static void kindHelper() {
-        for (String s:
-             words) {
-
-        }
-        System.out.println(words);
-    }
-    //Get next lexeme
-    public static void next() throws IOException {
-        try{
-            next();
-            System.out.println(position() + " " + kind() + " " + value());
-
-            while(!kind().equals("end-of-text")) {
-                next();
-                System.out.println(position() + " " + kind() + " " + value());
-            }
-        }
-        catch (IOException e) {
-            System.out.println("Could not fulfil the next method.\n" + e);
-        }
-    }
-
     //Get kind of lexeme
     public static String kind() {
         switch (currentKind) {
-            case "identifiers" -> currentKind = "ID";
-            case "integer" -> currentKind = "NUM";
-            case "keyword" -> currentKind = "program";
-            case "symbol" -> currentKind = ":=";
-            case "end" -> currentKind = "end-of-text";
+            case "identifiers" -> currentKind = "'ID'";
+            case "integer" -> currentKind = "'NUM'";
+            case "keyword", "symbol" -> {
+                return "'" + currentTokenRead + "'";
+            }
+            case "end" -> {
+                currentKind = "end-of-text";
+                System.out.println("Reached the end of the file.\n...\nResetting program\n\n");
+                sequenceKeepRunning();
+            }
         }
         return currentKind;
     }
-
     //Get value of lexeme if it is an ID or NUM
     public static String value() {
-        for (String s: words) {
-            if (isKeyword(s)) {
-                kind();
-            }
+        if (currentKind.equals("'ID'")) {
+            return currentTokenRead;
         }
-        if (currentKind.equals("identifiers")) {
-            return  "ID";
-        }
-        else if (currentKind.equals("integer")) {
-            return "NUM";
+        else if (currentKind.equals("'NUM'")) {
+            return currentTokenRead;
         }
         return currentTokenValue;
     }
-
     //Get position of lexeme
     public static String position() {return (currentLine) + ":" + (currentCharInLine);}
     //Check reserved keywords
-    public static boolean isKeyword(String tokenVar) {
-        String[] reservedKeyword = {"program", "bool", "int", "if", "else", "then", "fi", "not", "true", "false", "print", "while", "do", "od",
-        "<", "=<", "=", "!=", ">=", ">", "+", "-", "or", "*", "/", "and"};
-        //need multiple arrays for the different kinds of words.
-        System.out.println(position() + ": " + tokenVar);
-        for (String s : reservedKeyword) {
-            if (s.equals(tokenVar)) {
-                return true;
+    public static void isKeyword(String tokenVar) {
+        String[][] reservedKeyword = {
+                {"program", ":", ";", "bool", "int"},
+                {"<", "=<", "=", "!=", ">=", ">", "+", "-", "*", "/", ":="},
+                {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"},
+                {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0",}};
+
+        for(int i = 0; i < reservedKeyword.length; i++) {
+            for(int j = 0; j < reservedKeyword[i].length; j++) {
+                String arrayHost;
+                arrayHost = String.valueOf(reservedKeyword[i][j]);
+
+                if (i == 0) {
+                    if (tokenVar.equalsIgnoreCase(arrayHost)) {
+                        currentKind = "keyword";
+                        currentTokenRead = reservedKeyword[i][j];
+                    }
+                }
+                else if (i == 1) {
+                    if (tokenVar.equalsIgnoreCase(arrayHost)) {
+                        currentKind = "symbol";
+                        currentTokenRead = reservedKeyword[i][j];
+                    }
+                }
+                else if (i == 2) {
+                    if (tokenVar.equalsIgnoreCase(arrayHost)) {
+                        currentKind = "identifiers";
+                        currentTokenRead = reservedKeyword[i][j];
+                    }
+                }
+                else {
+                        currentKind = "integer";
+                        currentTokenRead = tokenVar;
+
+                }
             }
         }
-        return false;
+        if (tokenVar.equalsIgnoreCase("end")) {
+            currentKind = "end";
+        }
     }
-
     //Report syntax errors
-    public static boolean reportLexicalError(char c) {
+    public static void reportLexicalError(char c) {
         if(c == '@' || c == '#' || c == '$' || c == '%' || c == '^' || c == '&' || c == '`' || c == '~' || c == ',' || c == '\"' || c == '?' || c == '\'' || c == '[' || c == ']') {
             System.out.println("\nIllegal character at " + position() + ". Character is '" + c + "'.\nExiting program...");
             System.exit(0);
-            return lexErrorReported = true;
         }
-        return lexErrorReported;
     }
-
     //Function that keeps program running
-    public static void sequenceKeepRunning() throws IOException {
+    public static void sequenceKeepRunning() {
         try{
             reset();
             main(new String[0]);
@@ -185,14 +192,12 @@ public class lexicalAnalyzer {
             System.exit(0);
         }
     }
-
     //Reset variables to analyze next file
     private static void reset() {
         currentKind = "";
-        currentLine = 1;
+        currentLine = 0;
         currentCharInLine = 0;
         currentTokenValue = "";
         currentTokenRead = "";
-        lexErrorReported = false;
     }
 }
